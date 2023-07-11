d7018182dde3a1146b5c78c9896f540dfb512791
ainurjanat <ainurjanat@mail.ru>
2023-07-12 01:12:42 +0600

    Added Post model and Comment model

--- linkedin/models.py
+++ linkedin/models.py
@@ -11,3 +11,24 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts')
+    # user.liked_posts.all() for all liked posts
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_comments')
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
