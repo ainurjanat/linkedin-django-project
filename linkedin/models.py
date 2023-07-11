48f0883d9ee3267beac292dfca3c523df723b063
ainurjanat <ainurjanat@mail.ru>
2023-07-12 00:50:45 +0600

    Added User model in models.py

---
+++ linkedin/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    photo = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
